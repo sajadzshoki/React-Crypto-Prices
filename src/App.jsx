4ea688f641bdb4ea053d22ca63b11bb524ef2028
sajad shokraei <shockzbeatbox@gmail.com>
2023-03-12 20:48:36 +0330

    update (currency fixed , loader , styles)

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,22 @@ import Coin from "./components/Coin";
 import axios from "axios";
 
 function App() {
-  const [coins, setCoins] = React.useState([]);
+  const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
-
   // React.useEffect(() => {
   //   fetch(
-  //     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+  // "https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false"
   //   ).then((res) => {
   //     setCoins(res.data);
   //   });
   // }, []);
   useEffect(() => {
+    // if (!coins.length){
+    //   return "...loading"
+    // }
     axios
       .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false"
       )
       .then((res) => {
         setCoins(res.data);
@@ -31,7 +33,7 @@ function App() {
   const filteredCoins = coins.filter((coin) =>
     coin.name.toLowerCase().includes(search.toLowerCase())
   );
-
+  if (!coins.length) return <span class="loader"></span>;
   return (
     <div className="coin-app">
       <h1 className="header">Crypto Prices</h1>
@@ -45,7 +47,9 @@ function App() {
           />
         </form>
       </div>
-      <a href="javascript:location.reload(true)" className="refresh"><button>refresh</button></a>
+      <a href="javascript:location.reload(true)" className="refresh">
+        <button>refresh</button>
+      </a>
       {filteredCoins.map((coin) => {
         return (
           <div>
--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -1,29 +1,28 @@
 import React from "react";
 
-function Coin({ image, name, price, priceChange, marketCap ,rank }) {
-  
+function Coin({ image, name, price, priceChange, marketCap, rank }) {
   return (
-    
     <div className="container">
-      
-      <div className="coin-row">
-        <div className="coin">
-          <p className="rank">{rank} .</p>
-          <img src={image} className="logos"/>
-          <h2>{name}</h2>
-        </div>
-        <div className="coin-details">
-          <p className="price">{(price/80).toFixed(5)}</p>
-          {priceChange < 0 ? (
-            <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
-          ) : (
-            <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
-          )}
-        </div>
+      <a href={`https://coinmarketcap.com/currencies/${name}/`}>
+        <div className="coin-row">
+          <div className="coin">
+            <p className="rank">{rank} .</p>
+            <img src={image} className="logos" />
+            <h2>{name}</h2>
+          </div>
+          <div className="coin-details">
+            <p className="price">{price.toFixed(5)}</p>
+            {priceChange < 0 ? (
+              <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
+            ) : (
+              <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
+            )}
+          </div>
           <p className="coin-marketcap">
-            Market Cap :<br></br> {(marketCap/80).toLocaleString( )}
+            Market Cap :<br></br> {(marketCap / 80).toLocaleString()}
           </p>
-      </div>
+        </div>
+      </a>
     </div>
   );
 }
